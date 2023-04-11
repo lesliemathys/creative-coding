c4131a7b5b3259197ba86708216917fc07d9e80b
Leslie Mathys <dev@lesliemathys.com>
2023-04-11 21:36:38 +1000

    Amended keyPressed function

--- p5-circles/sketch.js
+++ p5-circles/sketch.js
@@ -1,7 +1,31 @@
+let bg = [32, 5, 97];
+
+let palette = [[203, 78, 90], [7, 78, 90], [45, 78, 90]];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
+  colorMode(HSB);
+  noLoop();
+  background(bg);
+  blendMode(MULTIPLY);
 }
 
 function draw() {
-  background(220);
+  noStroke();
+  translate(width/4,height/4);
+  for (let i = 0; i < 10; i++) {
+    fill(palette[colourSelector()])
+    circle(random(width-(width/2)), random(height-(height/2)),random(100,300));
+  }
+}
+
+function colourSelector() {
+  selector = floor(random(3));
+  return selector
+}
+
+function keyPressed(){
+  if (key === 's'){
+      save('circles.png');
+  }
 }
