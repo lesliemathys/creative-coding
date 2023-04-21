5e2d76b1344e7d393ec00171e38cc2ff67f3f5cd
Leslie Mathys <dev@lesliemathys.com>
2023-04-21 21:48:23 +1000

    Removed blob

--- p5-circles/sketch.js
+++ p5-circles/sketch.js
@@ -9,37 +9,34 @@ let palettes = [
   [[190, 78, 90], [10, 38, 90], [338, 39, 90]] //palette 3
 ];
 
+let rad;
+let res;
+let angle;
+
 function setup() {
-  //WIDTH = min(windowWidth, windowHeight);
-  WIDTH = 1000;
+  WIDTH = min(windowWidth, windowHeight);
+  //WIDTH = 1000;
   print(WIDTH);
   createCanvas(WIDTH, WIDTH);
+  angleMode(DEGREES);
   colorMode(HSB);
   noLoop();
   background(bg);
 
   paletteSelect = floor(random(palettes.length));
 
-  // for (let i = 0; i < random(3, 11); i++) {
-  //   bubbles[i] = new Bubble();
-  // }
-
-  for (let i = 0; i < floor(random(3, 11)); i++) {
-    blobs[i] = new Blob();
+  for (let i = 0; i < random(3, 11); i++) {
+    bubbles[i] = new Bubble();
   }
+
 }
 
 function draw() {
   push();
   translate(width / 4, height / 4);
-  // for (let i = 0; i < bubbles.length; i++) {
-  //   bubbles[i].show();
-  // }
-
-  for (let i = 0; i < blobs.length; i++) {
+  for (let i = 0; i < bubbles.length; i++) {
     bubbles[i].show();
   }
-
   pop();
 }
 
@@ -57,46 +54,4 @@ class Bubble {
     fill(palettes[paletteSelect][paletteColour]);    
     circle(this.x, this.y, this.d);
   }
-}
-
-class Blob {
-  constructor() {
-    this.x = [];
-    this.y = [];
-    this.cx;
-    this.cy;
-    this.r;
-    this.res;
-    this.slice;
-  }
-
-  params(_res, _cx, _cy) {
-    this.res = _res;
-
-    this.x = new [res];
-    this.y = new [res];
-
-    this.cx = _cx;
-    this.cy = _cy;
-
-    this. r = floor(random(10, 50));
-
-    slice = radians(360 / this.res);
-  }
-
-  init() {
-    for (int i = 0; i < res; i++) {
-      let angle = i * slice;
-      x[i] = cx + cos(angle) * r;
-      y[i] = cy + sin(angle) * r;
-    }
-  }
-
-  show() {
-    for (int i = 0; i < this.res; i++) {
-      curveVertex(x[i], y[i]);
-    }
-  }
-
-
 }
